e0897944f3c196a10092e62751e1fe382eea39b5
GABRIEL ALVES SCAVASSA <gascavassa@paschoalotto.com.br>
2017-04-11 12:56:45 -0300

    2º Commit - Gorilla Mux Implementation.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ My first Golang API.
 
 ### 1º Commit 
 Creating a rout handler with Golang basics.
+
+### 2º Commit
+We downloaded a GO package that help us to handle URL, the Gorilla Mux. 
+Gorilla Mux is package implements a request router and dispatcher (http://www.gorillatoolkit.org/pkg/mux).
+
+To download Gorilla Mux  use go get github.com/gorilla/mux
+This command will download the src to your GOPATH\src\github.com\gorilla folder, and when your project need this src, it will import from this destination folder. 
--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello!!, %q", html.EscapeString(r.URL.Path))
-	})
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+//Index rout handler
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello - We had modified our code!  %q", html.EscapeString(r.URL.Path))
 }
