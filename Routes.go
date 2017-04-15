57c4515d2f91bd603264c95f5405e61ef3cb5d9b
j-ew-s <gabriel.scavassa@outlook.com>
2017-04-15 09:37:22 -0300

    UPDATING: Adding to http.HandlerFunc the resulto of Handler from Logger

--- Routes.go
+++ Routes.go
@@ -18,7 +18,10 @@ type Routes []Route
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
-		router.Methods(route.Method).Path(route.Path).Name(route.Name).Handler(route.HandlerFunc)
+		var handler http.Handler
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+		router.Methods(route.Method).Path(route.Path).Name(route.Name).Handler(handler)
 	}
 	return router
 }
